67141e03d0e35b1f51cc9073eaadc76749b321b9
yysun316 <mikeyeung316@gmail.com>
2024-02-05 17:00:46 +0800

    Finished

--- script.js
+++ script.js
@@ -1,9 +1,67 @@
-var block = document.getElementById('block');
-var hole = document.getElementById('hole');
-var character = document.getElementById('character');
+var game = document.getElementById("game");
+var block = document.getElementById("block");
+var hole = document.getElementById("hole");
+var character = document.getElementById("character");
+var messageStart = document.getElementById("messageStart");
+var messageEnd = document.getElementById("messageEnd");
+var jumping = false;
+var counter = 0;
+var running = false;
+
+// Start Game
+game.addEventListener('click', function () {
+    running = true;
+    block.style.animationPlayState = "running";
+    hole.style.animationPlayState = "running";
+    character.style.animationPlayState = "running";
+    // Hide start message
+    messageStart.style.display = "none";
+    // Hide end message
+    messageEnd.style.display = "none";
+});
 
 hole.addEventListener('animationiteration', () => {
-    // hole.style.top will be range = [-150, -450]
-    var random = -((Math.random() * 300) + 150);
-    hole.style.top = random + "px";
+    if (running == true) {
+        var random = -((Math.random() * 300) + 150);
+        hole.style.top = random + "px";
+        counter++;
+    }
 });
+setInterval(function () {
+    if (running == true) {
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if (jumping == false) {
+            character.style.top = (characterTop + 3) + "px";
+        }
+        var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+        var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+        var cTop = -(500 - characterTop);
+        // Game over: 
+        if ((characterTop > 480) || // 1. Character hits the ground 
+            ((blockLeft < 20) && (blockLeft > -50) && ((cTop < holeTop) || (cTop > holeTop + 130)))) { // 2. Character hits the block
+            running = false;
+            alert("Game over. Score: " + (counter - 1));
+            messageEnd.style.display = "block";
+            character.style.top = 100 + "px";
+            count = 0;
+        }
+    }
+}, 10);
+
+function jump() {
+    jumping = true;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function () {
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if ((characterTop > 6) && (jumpCount < 15)) {
+            character.style.top = (characterTop - 5) + "px";
+        }
+        // allow character to fall down after jump
+        if (jumpCount > 20) {
+            clearInterval(jumpInterval);
+            jumping = false;
+            jumpCount = 0;
+        }
+        jumpCount++;
+    }, 10);
+}
